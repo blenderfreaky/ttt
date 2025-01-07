dec395d5a3efb1ebf2ec0bb33d83f0ad721bce26
blenderfreaky <blenderfreaky@mailbox.org>
2025-01-07 22:02:27 +0100

    testing

--- flake.nix
+++ flake.nix
@@ -5,10 +5,10 @@
     nixpkgs.url = "github:LunNova/nixpkgs/rocm-update";
 
     #flake-parts.url = "github:hercules-ci/flake-parts";
-    flakelight.url = "github:nix-community/flakelight";
+    #flakelight.url = "github:nix-community/flakelight";
   };
 
-  outputs = {flakelight, ...}:
+  outputs = {nixpkgs, ...}:
   #flake-parts.lib.mkFlake {inherit inputs;} {
   #  flake = {
   #  };
@@ -20,45 +20,50 @@
   #    system,
   #    ...
   #  }: let
-    flakelight ./. (let
-      rocm = pkgs:
-        pkgs.symlinkJoin {
-          name = "rocm-merged";
-          paths = with pkgs.rocmPackages; [
-            clr
-            rocm-core
-            rocm-device-libs
-            rocm-runtime
-          ];
-          buildInputs = [pkgs.makeWrapper];
-          postBuild = ''
-            wrapProgram $out/bin/hipcc \
-              --add-flags "--rocm-device-lib-path=$out/amdgcn/bitcode"
-          '';
-        };
-    in {
-      nixpkgs.config = {
-        rocmSupport = true;
+  #flakelight ./. (let
+  let
+    systems = ["x86_64-linux"];
+    forAllSys = nixpkgs.lib.genAttrs systems;
+    forAllSysPkgs = f: forAllSys (sys: f nixpkgs.legacyPackages.${sys});
+    rocm = pkgs:
+      pkgs.symlinkJoin {
+        name = "rocm-merged";
+        paths = with pkgs.rocmPackages; [
+          clr
+          rocm-core
+          rocm-device-libs
+          rocm-runtime
+        ];
+        buildInputs = [pkgs.makeWrapper];
+        postBuild = ''
+          wrapProgram $out/bin/hipcc \
+            --add-flags "--rocm-device-lib-path=$out/amdgcn/bitcode"
+        '';
       };
-      devShells.default = pkgs:
-        pkgs.mkShell {
-          packages = with pkgs; [(rocm pkgs) cmake ninja kokkos];
-        };
-
-      #devShells = {
-      #  kokkos = pkgs.mkShell {
-      #    packages = with pkgs; with pkgs.rocmPackages; [cmake kokkos clr hip-common];
-      #  };
-      #};
-
-      packages = pkgs:
-        (import ./nix {inherit pkgs;})
-        // {
-          ttt-kokkos = pkgs.stdenv.mkDerivation {
-            name = "kokkos";
-            src = ./kokkos;
-            buildInputs = with pkgs; [cmake kokkos (rocm pkgs)];
-          };
-        };
+  in {
+    nixpkgs.config = {
+      rocmSupport = true;
+    };
+    devShells = forAllSysPkgs (pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [(rocm pkgs) cmake ninja kokkos];
+      };
     });
+
+    #devShells = {
+    #  kokkos = pkgs.mkShell {
+    #    packages = with pkgs; with pkgs.rocmPackages; [cmake kokkos clr hip-common];
+    #  };
+    #};
+
+    packages = forAllSysPkgs (pkgs:
+      (import ./nix {inherit pkgs;})
+      // {
+        ttt-kokkos = pkgs.stdenv.mkDerivation {
+          name = "kokkos";
+          src = ./kokkos;
+          buildInputs = with pkgs; [cmake kokkos (rocm pkgs)];
+        };
+      });
+  };
 }
