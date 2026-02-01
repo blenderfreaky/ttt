a838e84796cafa5dfb2dff1535aa605b08d5b9ee
blenderfreaky <blenderfreaky@mailbox.org>
2026-02-01 17:07:54 +0100

    use het tests

--- thundercube/src/cube/tests/mma.rs
+++ thundercube/src/cube/tests/mma.rs
@@ -8,79 +8,74 @@ use crate::{
     test_kernel,
 };
 
-/// Test kernel that performs C = A * B using the mma_AtB function.
-///
-/// Layout:
-/// - A (in_a): Row-major [M, K] in global, loaded transposed into shared memory as [K, M]
-/// - B (in_b): Row-major [K, N]
-/// - C (output): Row-major [M, N]
-///
-/// The plane divides work among threads, with each thread computing a ThreadTileM x ThreadTileN
-/// sub-tile of the output.
+/// Heterogeneous test kernel for mma_AtB: FIn inputs, FAcc accumulator
 #[cube(launch)]
-fn test_mma_AtB<
-    F: Float,
+fn test_mma_AtB_hetero<
+    FIn: Float,
+    FAcc: Float,
     TileK: Dim,
     TileM: Dim,
     TileN: Dim,
     ThreadTileM: Dim,
     ThreadTileN: Dim,
 >(
+    in_a: &Tensor<Line<FIn>>,
+    in_b: &Tensor<Line<FIn>>,
+    output: &mut Tensor<Line<FAcc>>,
+) {
+    let mut st_a = St::<FIn, TileK, TileM>::new();
+    let mut st_b = St::<FIn, TileK, TileN>::new();
+
+    let mut rt_c = Rt::<FAcc, ThreadTileM, ThreadTileN>::new();
+    rt_c.zero();
+
+    load_st_transpose(in_a, &mut st_a, 0, 0, 0);
+    load_st_direct(in_b, &mut st_b, 0, 0, 0);
+
+    mma_AtB::<FIn, FAcc, TileM, TileK, TileN, ThreadTileM, ThreadTileN>(&mut rt_c, &st_a, &st_b);
+
+    store_rt_direct::<FAcc, ThreadTileM, ThreadTileN, TileM, TileN>(&rt_c, output, 0, 0, 0);
+}
+
+/// Test kernel that performs C = A * B using the mma_AtB function.
+#[cube(launch)]
+fn test_mma_AtB<F: Float, TileK: Dim, TileM: Dim, TileN: Dim, ThreadTileM: Dim, ThreadTileN: Dim>(
     in_a: &Tensor<Line<F>>,
     in_b: &Tensor<Line<F>>,
     output: &mut Tensor<Line<F>>,
 ) {
-    // st_a is [K, M] - loaded from A which is [M, K] via transpose
-    let mut st_a = St::<F, TileK, TileM>::new();
-    // st_b is [K, N]
-    let mut st_b = St::<F, TileK, TileN>::new();
+    test_mma_AtB_hetero::<F, F, TileK, TileM, TileN, ThreadTileM, ThreadTileN>(in_a, in_b, output);
+}
 
-    let mut rt_c = Rt::<F, ThreadTileM, ThreadTileN>::new();
+/// Heterogeneous test kernel for mma_AB: FIn inputs, FAcc accumulator
+#[cube(launch)]
+fn test_mma_AB_hetero<FIn: Float, FAcc: Float, TileM: Dim, TileK: Dim, TileN: Dim, ThreadTileM: Dim, ThreadTileN: Dim>(
+    in_a: &Tensor<Line<FIn>>,
+    in_b: &Tensor<Line<FIn>>,
+    output: &mut Tensor<Line<FAcc>>,
+) {
+    let mut st_a = St::<FIn, TileM, TileK>::new();
+    let mut st_b = St::<FIn, TileK, TileN>::new();
+
+    let mut rt_c = Rt::<FAcc, ThreadTileM, ThreadTileN>::new();
     rt_c.zero();
 
-    // Load A as [M, K], transpose to [K, M] in shared memory
-    load_st_transpose(in_a, &mut st_a, 0, 0, 0);
-    // Load B as [K, N] directly
+    load_st_direct(in_a, &mut st_a, 0, 0, 0);
     load_st_direct(in_b, &mut st_b, 0, 0, 0);
 
-    mma_AtB::<F, F, TileM, TileK, TileN, ThreadTileM, ThreadTileN>(&mut rt_c, &st_a, &st_b);
+    mma_AB::<FIn, FAcc, TileM, TileK, TileN, ThreadTileM, ThreadTileN>(&mut rt_c, &st_a, &st_b);
 
-    // Store directly - function computes per-thread offsets automatically
-    store_rt_direct::<F, ThreadTileM, ThreadTileN, TileM, TileN>(&rt_c, output, 0, 0, 0);
+    store_rt_direct::<FAcc, ThreadTileM, ThreadTileN, TileM, TileN>(&rt_c, output, 0, 0, 0);
 }
 
 /// Test kernel that performs C = A * B using the mma_AB function.
-///
-/// Layout:
-/// - A (in_a): Row-major [M, K] in global, loaded directly into shared memory as [M, K]
-/// - B (in_b): Row-major [K, N]
-/// - C (output): Row-major [M, N]
-///
-/// The plane divides work among threads, with each thread computing a ThreadTileM x ThreadTileN
-/// sub-tile of the output.
 #[cube(launch)]
 fn test_mma_AB<F: Float, TileM: Dim, TileK: Dim, TileN: Dim, ThreadTileM: Dim, ThreadTileN: Dim>(
     in_a: &Tensor<Line<F>>,
     in_b: &Tensor<Line<F>>,
     output: &mut Tensor<Line<F>>,
 ) {
-    // st_a is [M, K] - loaded directly from A which is [M, K]
-    let mut st_a = St::<F, TileM, TileK>::new();
-    // st_b is [K, N]
-    let mut st_b = St::<F, TileK, TileN>::new();
-
-    let mut rt_c = Rt::<F, ThreadTileM, ThreadTileN>::new();
-    rt_c.zero();
-
-    // Load A as [M, K] directly
-    load_st_direct(in_a, &mut st_a, 0, 0, 0);
-    // Load B as [K, N] directly
-    load_st_direct(in_b, &mut st_b, 0, 0, 0);
-
-    mma_AB::<F, F, TileM, TileK, TileN, ThreadTileM, ThreadTileN>(&mut rt_c, &st_a, &st_b);
-
-    // Store directly - function computes per-thread offsets automatically
-    store_rt_direct::<F, ThreadTileM, ThreadTileN, TileM, TileN>(&rt_c, output, 0, 0, 0);
+    test_mma_AB_hetero::<F, F, TileM, TileK, TileN, ThreadTileM, ThreadTileN>(in_a, in_b, output);
 }
 
 test_kernel! {
@@ -137,6 +132,72 @@ test_kernel! {
                         for ki in 0..TileK::VALUE {
                             let a_val = in_a[mi * TileK::VALUE + ki];
                             let b_val = in_b[ki * TileN::VALUE + ni];
+                            sum += a_val * b_val;
+                        }
+                        output[mi * TileN::VALUE + ni] = sum;
+                    }
+                }
+            }
+        );
+    }
+}
+
+use crate::test_utils::TestFloat;
+
+test_kernel! {
+    #[test]
+    fn test_mma_AB_hetero() for
+        (FIn, FAcc) in [(bf16, f32), (f16, f32)]
+        TileM in [D8]
+        TileK in [D4, D8]
+        TileN in [D8]
+        ThreadTileM in [D4]
+        ThreadTileN in [D4]
+    {
+        let in_a: Tensor<FIn> = [TileM::VALUE, TileK::VALUE] as Range;
+        let in_b: Tensor<FIn> = [TileK::VALUE, TileN::VALUE] as Range;
+        let output: Tensor<FAcc> = [TileM::VALUE, TileN::VALUE];
+
+        assert_eq!(
+            test_mma_AB_hetero(in_a(), in_b(), output()) for (1, 1, 1) @ ((TileM::VALUE / ThreadTileM::VALUE) * (TileN::VALUE / ThreadTileN::VALUE)),
+            {
+                for mi in 0..TileM::VALUE {
+                    for ni in 0..TileN::VALUE {
+                        let mut sum = FAcc::from_int(0);
+                        for ki in 0..TileK::VALUE {
+                            let a_val = FAcc::from_f64(in_a[mi * TileK::VALUE + ki].into_f64());
+                            let b_val = FAcc::from_f64(in_b[ki * TileN::VALUE + ni].into_f64());
+                            sum += a_val * b_val;
+                        }
+                        output[mi * TileN::VALUE + ni] = sum;
+                    }
+                }
+            }
+        );
+    }
+
+    #[test]
+    fn test_mma_AtB_hetero() for
+        (FIn, FAcc) in [(bf16, f32), (f16, f32)]
+        TileK in [D4, D8]
+        TileM in [D8]
+        TileN in [D8]
+        ThreadTileM in [D4]
+        ThreadTileN in [D4]
+    {
+        let in_a: Tensor<FIn> = [TileM::VALUE, TileK::VALUE] as Range;
+        let in_b: Tensor<FIn> = [TileK::VALUE, TileN::VALUE] as Range;
+        let output: Tensor<FAcc> = [TileM::VALUE, TileN::VALUE];
+
+        assert_eq!(
+            test_mma_AtB_hetero(in_a(), in_b(), output()) for (1, 1, 1) @ ((TileM::VALUE / ThreadTileM::VALUE) * (TileN::VALUE / ThreadTileN::VALUE)),
+            {
+                for mi in 0..TileM::VALUE {
+                    for ni in 0..TileN::VALUE {
+                        let mut sum = FAcc::from_int(0);
+                        for ki in 0..TileK::VALUE {
+                            let a_val = FAcc::from_f64(in_a[mi * TileK::VALUE + ki].into_f64());
+                            let b_val = FAcc::from_f64(in_b[ki * TileN::VALUE + ni].into_f64());
                             sum += a_val * b_val;
                         }
                         output[mi * TileN::VALUE + ni] = sum;
