8e4b563416708fcb18d6ed4607f4f39233d48b30
kilyanni <github@nkilian.org>
2026-02-08 01:04:28 +0100

    fixup

--- crates/fused/src/linear_fused_tile/tests.rs
+++ crates/fused/src/linear_fused_tile/tests.rs
@@ -3,7 +3,9 @@
 use test_case::test_case;
 use ttt_core::{
     GpuAutodiffBackend, GpuBackend, TTTInnerModel, TTTLinearState,
-    test_utils::{TestDims, generate_test_inputs, test_backward_fmb, test_backward_fwd, test_fmb, test_fwd},
+    test_utils::{
+        TestDims, generate_test_inputs, test_backward_fmb, test_backward_fwd, test_fmb, test_fwd,
+    },
 };
 
 use crate::{FusedTile, FusedTileMulti};
@@ -63,37 +65,6 @@ fn test_fused_tile_multi_forward_vs_reference(
     );
 }
 
-#[test_case(2, 2, 32, 8 ; "batch2_heads2_dim32_seq8")]
-fn test_fused_tile_multi_fmb_vs_reference(batch: usize, heads: usize, dim: usize, seq: usize) {
-    let dims = TestDims::new(batch, heads, dim, seq);
-    test_fmb::<GpuBackend, FusedTileMulti<GpuBackend>, TTTLinearState<GpuBackend>, _>(
-        dims,
-        |m| m.into(),
-        RTOL,
-        ATOL,
-        "FusedTileMulti",
-    );
-}
-
-#[test_case(2, 2, 32, 8, 2 ; "batch2_heads2_dim32_mini8_stages2")]
-#[ignore] // forward_mini_batch with full seq_len is incompatible with TileMulti wrapper
-fn test_fused_tile_multi_fmb_backward_vs_reference(
-    batch: usize,
-    heads: usize,
-    dim: usize,
-    mini_batch: usize,
-    stages: usize,
-) {
-    let dims = TestDims::multi_stage(batch, heads, dim, mini_batch, stages);
-    test_backward_fmb::<GpuAutodiffBackend, FusedTileMulti<GpuAutodiffBackend>, _>(
-        dims,
-        |m| m.into(),
-        BACKWARD_RTOL,
-        BACKWARD_ATOL,
-        "FusedTileMulti",
-    );
-}
-
 #[test_case(2, 2, 32, 8, 1, 1 ; "batch2_heads2_dim32_mini8_stages1_ckpt1")]
 #[test_case(2, 2, 32, 8, 2, 1 ; "batch2_heads2_dim32_mini8_stages2_ckpt1")]
 #[test_case(2, 2, 32, 8, 4, 2 ; "batch2_heads2_dim32_mini8_stages4_ckpt2")]
@@ -130,7 +101,13 @@ fn test_fused_tile_multi_backward_vs_reference(
 #[test_case(1, 12, 64, 1024, 8 ; "125m_seq1024_ckpt8")]
 #[test_case(1, 12, 64, 2048, 16 ; "125m_seq2048_ckpt16")]
 #[ignore]
-fn bench_fused_tile_multi(batch: usize, heads: usize, dim: usize, seq: usize, ckpt_interval: usize) {
+fn bench_fused_tile_multi(
+    batch: usize,
+    heads: usize,
+    dim: usize,
+    seq: usize,
+    ckpt_interval: usize,
+) {
     use burn::prelude::Backend;
 
     let device: <GpuBackend as Backend>::Device = Default::default();
@@ -177,8 +154,11 @@ fn bench_fused_tile_multi(batch: usize, heads: usize, dim: usize, seq: usize, ck
     );
     eprintln!(
         "[BENCH] {} iters, {:.1} us/iter ({:.3} ms/iter), {:.1} us/stage ({:.3} ms/stage), total {:.1} ms",
-        iterations, per_iter_us, per_iter_us / 1000.0,
-        per_stage_us, per_stage_us / 1000.0,
+        iterations,
+        per_iter_us,
+        per_iter_us / 1000.0,
+        per_stage_us,
+        per_stage_us / 1000.0,
         elapsed.as_secs_f64() * 1000.0,
     );
 }
